375cf5d42dbc7cdeb844be52a9a1303426ff4f15
mzwandile.mahlangu <mzwandile.mahlangu@entelect.co.za>
2018-03-04 18:16:41 +0200

    Select a new joke every second from a list of jokes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Joke } from './models/joke';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  jokes: Joke[] = [
+    new Joke("Suger-free", "What do you call someone who can't stick with a diet?","A desserter."),
+    new Joke("That's deep", "All these sea monster jokes are just Kraken me up.", ""),
+    new Joke("LOL-ing in the deep", "My computer suddenly started belting out \"Someone Like You.\"","It's a Dell")   
+  ];
+  selectedJoke: Joke;
+  selectedJokeIndex: number;
+  refreshInterval: number;
+
+  constructor () {
+    this.selectNewJoke();
+    this.refreshInterval = 10000;
+    setInterval(() => { this.selectNewJoke(); }, this.refreshInterval);
+    
+  }
+
+  selectNewJoke() {
+    this.selectedJokeIndex = this.generateRandomInteger(0,2);
+    this.selectedJoke = this.jokes[this.selectedJokeIndex];
+  }
+
+  generateRandomInteger(min, max) {
+    return Math.floor(min + Math.random()*(max+1 - min))
+  }
 }
---
+++ src/app/models/joke.ts
@@ -0,0 +1,12 @@
+export class Joke {
+    public title: string;
+    public setup: string;
+    public punchLine: string;
+
+    constructor(title: string, setup: string, punchLine: string){
+      this.title = title;
+      this.setup = setup;
+      this.punchLine = punchLine;
+    }
+
+}
